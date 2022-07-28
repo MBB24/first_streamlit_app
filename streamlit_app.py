423823c064f1028970d6d61bda645a0c9107fcb1
MBB24 <110131253+MBB24@users.noreply.github.com>
2022-07-28 10:09:27 -0300

    Update streamlit_app.py
    Title and Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new Healthy dinner')
+streamlit.header('BREAKFAST MENU')
+Streamlit.text('Omega 3 & Blueberry oatmeal')
+Streamlit.text('Kale,Spinach & Rocket Smoothie')
+Streamlit.text('Hard-Boiled Free-Range Egg')
