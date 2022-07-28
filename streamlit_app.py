3bc7dfd0acbda6ae5cf0e77f9202dbe92c6251fd
MBB24 <110131253+MBB24@users.noreply.github.com>
2022-07-28 10:16:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('My Parents new Healthy dinner')
 streamlit.header('BREAKFAST MENU')
 streamlit.text('Omega3 & Blueberry Otmeal')
+streamlit.text('Kalet,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
