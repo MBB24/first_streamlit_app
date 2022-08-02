e0bfb85661eb472bbc96d31b018b81ef42e556ec
MBB24 <110131253+MBB24@users.noreply.github.com>
2022-08-02 17:28:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ streamlit.dataframe(Fruits_to_show)
 
 #New section to display fruitvice api response}
 streamlit.header("Fruityvice Fruit Advice!")
-
+#Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
